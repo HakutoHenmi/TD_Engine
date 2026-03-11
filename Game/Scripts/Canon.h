5e2757dced8dc9f94fca5a4d7ab3aff71884c2f5
Master Fujii <k024g0078@g.neec.ac.jp>
2026-03-11 12:46:01 +0900

    a

--- Game/Scripts/Canon.h
+++ Game/Scripts/Canon.h
@@ -12,14 +12,14 @@ public:
 
 private:
 	// 大砲性能
-	float attackRange_ = 50.0f;    // 攻撃範囲
+	float attackRange_ = 50.0f;   // 攻撃範囲
 	float attackInterval_ = 1.0f; // 攻撃間隔（秒）
 	float damage_ = 10.0f;        // ダメージ量
 	                              // クールダウン
 	float attackTimer_ = 0.0f;
 	float rotationSpeed_ = 1.0f; // タワーの回転速度（ラジアン/秒）
 
-		int objectCount = 0;
+	int objectCount = 0;
 	int pipeCount = 0;
 	int enemyCount = 0;
 };
