6101baaef4ca8fed7f405ed5edf0bd2bec408c0f
剛 増谷 <K024G0075@g.neec.ac.jp>
2026-03-16 10:10:34 +0900

    キリのいいとこ

--- Game/Scripts/EnemyBehavior.h
+++ Game/Scripts/EnemyBehavior.h
@@ -61,7 +61,7 @@ private:
 	void SearchTarget(SceneObject& obj, GameScene* scene);
 
 	// オブジェクトの移動処理
-	void Move(SceneObject& obj, float dt);
+	void Move(SceneObject& obj, GameScene* scene, float dt);
 
 	// オブジェクトの周囲をチェックする関数
 	void ScanSurround(SceneObject& obj, GameScene* scene);
@@ -96,6 +96,7 @@ private: // メンバ変数
 	DirectX::XMFLOAT3 myPos_ = {};
 	float groundHeight_ = 0.0f;	// FlyTypeが地面の高さを取るため
 	DirectX::XMFLOAT3 targetPos_ = {}; // 移動速度
+	float totalTime_ = 0.0f;
 	float speed_ = 5.0f;
 
 	// グリッド関連
