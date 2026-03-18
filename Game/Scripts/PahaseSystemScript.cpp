e5d146d4e7746abf43c67354a9fef37b1f328474
matcha <k024g0024@g.neec.ac.jp>
2026-03-18 12:06:18 +0900

    　　

--- Game/Scripts/PahaseSystemScript.cpp
+++ Game/Scripts/PahaseSystemScript.cpp
@@ -1,15 +1,15 @@
 #include "PahaseSystemScript.h"
+#include "Editor/EditorUI.h"
 #include "ObjectTypes.h"
 #include "Scenes/GameScene.h"
 #include "ScriptEngine.h"
-#include "Editor/EditorUI.h"
-#include <imgui.h>
 #include <cmath>
+#include <imgui.h>
 #include <iostream>
 
 namespace Game {
 
-	bool PahaseSystemScript::isPreparation_ = false;
+bool PahaseSystemScript::isPreparation_ = false;
 
 void PahaseSystemScript::Start(SceneObject& obj, GameScene* scene) {
 	(obj);
@@ -29,7 +29,7 @@ void PahaseSystemScript::Update(SceneObject& obj, GameScene* scene, float dt) {
 
 	if (isPreparation_) {
 		Installation(scene, gameImageMin, tW, tH);
-		
+
 		if (keyP && !preKeyP_) {
 			isPreparation_ = false;
 		}
@@ -43,100 +43,70 @@ void PahaseSystemScript::Update(SceneObject& obj, GameScene* scene, float dt) {
 }
 
 void PahaseSystemScript::Installation(GameScene* scene, const ImVec2& gameImageMin, float tW, float tH) {
-	static bool preKey1 = false;
-	static bool placeMode = false; // 1 押下後にクリック待ちするモード
+	(gameImageMin);
+	(tW);
+	(tH);
 
-	bool key1 = (GetAsyncKeyState('1') & 0x8000) != 0;
-	bool mouseLeft = (GetAsyncKeyState(VK_LBUTTON) & 0x8000) != 0;
-	static bool prevMouseLeft = false;
+	auto* input = Engine::Input::GetInstance();
 
-	if (!scene || !scene->GetRenderer()) {
-		preKey1 = key1;
-		prevMouseLeft = mouseLeft;
-		return;
-	}
+	if (input->IsMouseTrigger(0)) {
+		float mx, my;
+		input->GetMousePos(mx, my);
 
-	// 1 キー押下で「次のクリックで設置」モード ON
-	if (key1 && !preKey1) {
-		placeMode = true;
-	}
+		// 1. カメラ情報の取得
+		auto& camera = scene->GetCamera();
+		DirectX::XMMATRIX view = camera.View();
+		DirectX::XMMATRIX proj = camera.Proj();
 
-	auto* renderer = scene->GetRenderer();
+		// View-Projectionの逆行列を計算
+		DirectX::XMMATRIX invVP = DirectX::XMMatrixInverse(nullptr, view * proj);
 
-	// Editor 上と同じように ImGui のゲームビュー座標からレイを飛ばす
-	ImVec2 mousePos = ImGui::GetMousePos();
-	float localX = mousePos.x - gameImageMin.x;
-	float localY = mousePos.y - gameImageMin.y;
-	bool insideImage = (localX >= 0 && localY >= 0 && localX <= tW && localY <= tH);
+		// 2. スクリーン座標を NDC (Normalized Device Coordinates) に変換 (-1.0 ～ 1.0)
+		float nx = (mx / (float)Engine::WindowDX::kW) * 2.0f - 1.0f;
+		float ny = 1.0f - (my / (float)Engine::WindowDX::kH) * 2.0f;
 
-	Engine::Vector3 hitPoint = {0, 0, 0};
-	bool hitTerrain = false;
+		// 3. レイの始点（近クリップ面）と終点（遠クリップ面）をワールド座標で求める
+		DirectX::XMVECTOR nearPointNDC = DirectX::XMVectorSet(nx, ny, 0.0f, 1.0f);
+		DirectX::XMVECTOR farPointNDC = DirectX::XMVectorSet(nx, ny, 1.0f, 1.0f);
 
-	if (insideImage) {
-		auto& cam = scene->GetCamera();
-		auto viewMat = cam.View();
-		auto projMat = cam.Proj();
-		DirectX::XMVECTOR rayOrig, rayDir;
-		EditorUI::ScreenToWorldRay(localX, localY, tW, tH, viewMat, projMat, rayOrig, rayDir);
+		DirectX::XMVECTOR posNear = DirectX::XMVector3TransformCoord(nearPointNDC, invVP);
+		DirectX::XMVECTOR posFar = DirectX::XMVector3TransformCoord(farPointNDC, invVP);
+		DirectX::XMVECTOR rayDir = DirectX::XMVector3Normalize(posFar - posNear);
 
-		float bestDist = FLT_MAX;
+		// 4. 地面 (y=0 の平面) との交差判定
+		DirectX::XMFLOAT3 pNear, d;
+		DirectX::XMStoreFloat3(&pNear, posNear);
+		DirectX::XMStoreFloat3(&d, rayDir);
 
-		auto objects = scene->GetObjects();
-		for (auto& obj : objects) {
-			bool isTerrain = (obj.name.find("Terrain") != std::string::npos) || (obj.name.find("Floor") != std::string::npos);
-			if (!isTerrain) continue;
+		// レイが下を向いている場合のみ判定（平行な場合は除外）
+		if (std::abs(d.y) > 0.0001f) {
+			// pNear.y + d.y * t = 0  =>  t = -pNear.y / d.y
+			float t = -pNear.y / d.y;
 
-			Engine::Model* model = nullptr;
-			if (!obj.gpuMeshColliders.empty()) {
-				model = renderer->GetModel(obj.gpuMeshColliders[0].meshHandle);
-			}
-			if (!model && obj.modelHandle != 0) {
-				model = renderer->GetModel(obj.modelHandle);
-			}
-			if (!model && !obj.meshRenderers.empty() && obj.meshRenderers[0].modelHandle != 0) {
-				model = renderer->GetModel(obj.meshRenderers[0].modelHandle);
-			}
-			if (model) {
-				float d; Engine::Vector3 hp;
-				if (model->RayCast(rayOrig, rayDir, obj.GetTransform().ToMatrix(), d, hp)) {
-					if (d < bestDist) {
-						bestDist = d;
-						hitPoint = hp;
-						hitTerrain = true;
-					}
+			if (t > 0) { // カメラより前方にある場合
+				DirectX::XMFLOAT3 hitPos;
+				hitPos.x = pNear.x + d.x * t;
+				hitPos.y = pNear.y + d.y * t;
+				hitPos.z = pNear.z + d.z * t;
+
+				// 5. オブジェクトの生成
+				SceneObject newObj;
+				newObj.name = "PlacedObject";
+				newObj.translate = hitPos;
+				newObj.scale = {1.0f, 1.0f, 1.0f};
+				newObj.color = {1.0f, 1.0f, 1.0f, 1.0f};
+
+				// モデルやテクスチャを設定する場合の例
+				auto* renderer = scene->GetRenderer();
+				if (renderer) {
+					newObj.modelHandle = renderer->LoadObjMesh("Resources/cube/cube.obj");
+					newObj.textureHandle = renderer->LoadTexture2D("Resources/white1x1.png");
 				}
+
+				scene->SpawnObject(newObj);
 			}
 		}
 	}
-
-	// placeMode 中に、左クリックの立ち上がり かつ 地面ヒット で設置
-	if (placeMode && hitTerrain && mouseLeft && !prevMouseLeft) {
-		SceneObject obj;
-		obj.name = "New Object";
-		obj.modelPath = "Resources/cube/cube.obj";
-		obj.texturePath = "Resources/white1x1.png";
-		obj.translate = { hitPoint.x, hitPoint.y + 0.5f, hitPoint.z };
-
-		HealthComponent health;
-		health.hp = 100.0f;
-		health.maxHp = 100.0f;
-		obj.healths.push_back(health);
-
-		MeshRendererComponent mr;
-		mr.modelPath = obj.modelPath;
-		mr.texturePath = obj.texturePath;
-		mr.modelHandle = renderer->LoadObjMesh(mr.modelPath);
-		mr.textureHandle = renderer->LoadTexture2D(mr.texturePath);
-		obj.meshRenderers.push_back(mr);
-
-		scene->SpawnObject(obj);
-
-		// 一度設置したらモード解除
-		placeMode = false;
-	}
-
-	preKey1 = key1;
-	prevMouseLeft = mouseLeft;
 }
 
 void PahaseSystemScript::OnDestroy(SceneObject& /*obj*/, GameScene* /*scene*/) {}
