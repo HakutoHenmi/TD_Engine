cf8b54b3d640626ffc43f53c414a46916920da0c
Masat <Masat@MATCHA>
2026-03-21 15:46:28 +0900

    複数出せるようにした

--- Game/Scripts/PahaseSystemScript.cpp
+++ Game/Scripts/PahaseSystemScript.cpp
@@ -22,102 +22,158 @@ void PahaseSystemScript::Update(SceneObject& obj, GameScene* scene, float dt) {
 	(scene);
 	(dt);
 	bool keyP = (GetAsyncKeyState('P') & 0x8000) != 0;
+	bool key1 = (GetAsyncKeyState('1') & 0x8000) != 0;
+	bool key2 = (GetAsyncKeyState('2') & 0x8000) != 0;
 
 	if (isPreparation_) {
-		Installation(scene);
+        if (key1 && !preKey1_) {
+			selectedObjPath_ = "Resources/cube/cube.obj";
+			isPlacementMode_ = true;
+		}
+
+		if (key2 && !preKey2_) {
+			selectedObjPath_ = "Resources/Cylinder/cylinder.obj";
+			isPlacementMode_ = true;
+		}
+
+        Installation(scene, selectedObjPath_);
 
 		if (keyP && !preKeyP_) {
 			isPreparation_ = false;
+            isPlacementMode_ = false;
 		}
 
 	} else {
+       isPlacementMode_ = false;
 		if (keyP && !preKeyP_) {
 			isPreparation_ = true;
 		}
 	}
 	preKeyP_ = keyP;
+ preKey1_ = key1;
+	preKey2_ = key2;
 }
 
-void PahaseSystemScript::Installation(GameScene* scene) {
+void PahaseSystemScript::Installation(GameScene* scene, const std::string& objPath) {
+	if (!isPlacementMode_) return;
+
 	auto* input = Engine::Input::GetInstance();
+	Engine::Vector3 hitPoint{};
+	if (!TryGetTerrainHitPoint(scene, hitPoint)) return;
 
-	// 左クリックが押された瞬間
+  DrawPlacementPreview(scene, hitPoint, objPath);
+
 	if (input->IsMouseTrigger(0)) {
-       ImVec2 mousePos = ImGui::GetMousePos();
-		ImVec2 gameMin = EditorUI::GetGameImageMin();
-		ImVec2 gameMax = EditorUI::GetGameImageMax();
-		float tW = gameMax.x - gameMin.x;
-		float tH = gameMax.y - gameMin.y;
-		if (tW <= 0.0f || tH <= 0.0f) return;
+       SpawnPlacedObject(scene, hitPoint, objPath);
+		isPlacementMode_ = false;
+	}
+}
 
-		float localX = mousePos.x - gameMin.x;
-		float localY = mousePos.y - gameMin.y;
-		bool insideImage = (localX >= 0.0f && localY >= 0.0f && localX <= tW && localY <= tH);
-		if (!insideImage) return;
+bool PahaseSystemScript::TryGetTerrainHitPoint(GameScene* scene, Engine::Vector3& outHitPoint) const {
+	ImVec2 mousePos = ImGui::GetMousePos();
+	ImVec2 gameMin = EditorUI::GetGameImageMin();
+	ImVec2 gameMax = EditorUI::GetGameImageMax();
+	float tW = gameMax.x - gameMin.x;
+	float tH = gameMax.y - gameMin.y;
+	if (tW <= 0.0f || tH <= 0.0f) return false;
 
-      auto& camera = scene->GetCamera();
-		DirectX::XMMATRIX view = camera.View();
-		DirectX::XMMATRIX proj = camera.Proj();
+	float localX = mousePos.x - gameMin.x;
+	float localY = mousePos.y - gameMin.y;
+	bool insideImage = (localX >= 0.0f && localY >= 0.0f && localX <= tW && localY <= tH);
+	if (!insideImage) return false;
 
-		DirectX::XMVECTOR rayOrig, rayDir;
-      EditorUI::ScreenToWorldRay(localX, localY, tW, tH, view, proj, rayOrig, rayDir);
+	auto& camera = scene->GetCamera();
+	DirectX::XMMATRIX view = camera.View();
+	DirectX::XMMATRIX proj = camera.Proj();
 
-		auto* renderer = scene->GetRenderer();
-		if (!renderer) return;
+	DirectX::XMVECTOR rayOrig, rayDir;
+	EditorUI::ScreenToWorldRay(localX, localY, tW, tH, view, proj, rayOrig, rayDir);
 
-		float bestDist = FLT_MAX;
-		Engine::Vector3 hitPoint = {0, 0, 0};
-		bool hitTerrain = false;
+	auto* renderer = scene->GetRenderer();
+	if (!renderer) return false;
 
-		const auto& objects = scene->GetObjects();
-		for (const auto& obj : objects) {
-			bool isTerrain = (obj.name.find("Terrain") != std::string::npos) || (obj.name.find("Floor") != std::string::npos);
-			if (!isTerrain) continue;
+	float bestDist = FLT_MAX;
+	bool hitTerrain = false;
 
-			Engine::Model* model = nullptr;
-			if (!obj.gpuMeshColliders.empty() && obj.gpuMeshColliders[0].meshHandle != 0) {
-				model = renderer->GetModel(obj.gpuMeshColliders[0].meshHandle);
-			}
-			if (!model && obj.modelHandle != 0) {
-				model = renderer->GetModel(obj.modelHandle);
-			}
-			if (!model && !obj.meshRenderers.empty() && obj.meshRenderers[0].modelHandle != 0) {
-				model = renderer->GetModel(obj.meshRenderers[0].modelHandle);
-			}
+	const auto& objects = scene->GetObjects();
+	for (const auto& obj : objects) {
+		bool isTerrain = (obj.name.find("Terrain") != std::string::npos) || (obj.name.find("Floor") != std::string::npos);
+		if (!isTerrain) continue;
 
-			if (!model) continue;
-
-			float d;
-			Engine::Vector3 hp;
-			if (model->RayCast(rayOrig, rayDir, obj.GetTransform().ToMatrix(), d, hp) && d < bestDist) {
-				bestDist = d;
-				hitPoint = hp;
-				hitTerrain = true;
-			}
+		Engine::Model* model = nullptr;
+		if (!obj.gpuMeshColliders.empty() && obj.gpuMeshColliders[0].meshHandle != 0) {
+			model = renderer->GetModel(obj.gpuMeshColliders[0].meshHandle);
+		}
+		if (!model && obj.modelHandle != 0) {
+			model = renderer->GetModel(obj.modelHandle);
+		}
+		if (!model && !obj.meshRenderers.empty() && obj.meshRenderers[0].modelHandle != 0) {
+			model = renderer->GetModel(obj.meshRenderers[0].modelHandle);
 		}
 
-		if (!hitTerrain) return;
+		if (!model) continue;
 
-		SceneObject newObj;
-		newObj.name = "PlacedCube";
-		newObj.translate = {hitPoint.x, hitPoint.y + 0.5f, hitPoint.z};
-		newObj.scale = {1.0f, 1.0f, 1.0f};
-		newObj.color = {1.0f, 1.0f, 1.0f, 1.0f};
-		newObj.modelPath = "Resources/cube/cube.obj";
-		newObj.texturePath = "Resources/white1x1.png";
-		newObj.modelHandle = renderer->LoadObjMesh(newObj.modelPath);
-		newObj.textureHandle = renderer->LoadTexture2D(newObj.texturePath);
-
-		MeshRendererComponent mr;
-		mr.modelHandle = newObj.modelHandle;
-		mr.textureHandle = newObj.textureHandle;
-		mr.modelPath = newObj.modelPath;
-		mr.texturePath = newObj.texturePath;
-		mr.shaderName = "Toon";
-		newObj.meshRenderers.push_back(mr);
-
-		scene->SpawnObject(newObj);
+		float d;
+		Engine::Vector3 hp;
+		if (model->RayCast(rayOrig, rayDir, obj.GetTransform().ToMatrix(), d, hp) && d < bestDist) {
+			bestDist = d;
+			outHitPoint = hp;
+			hitTerrain = true;
+		}
 	}
+
+	return hitTerrain;
+}
+
+void PahaseSystemScript::DrawPlacementPreview(GameScene* scene, const Engine::Vector3& hitPoint, const std::string& objPath) {
+	auto* renderer = scene->GetRenderer();
+	if (!renderer) return;
+
+ if (previewModelHandle_ == 0 || previewObjPath_ != objPath) {
+		previewModelHandle_ = renderer->LoadObjMesh(objPath);
+		previewObjPath_ = objPath;
+	}
+	if (previewTextureHandle_ == 0) {
+		previewTextureHandle_ = renderer->LoadTexture2D("Resources/white1x1.png");
+	}
+
+	Engine::Transform tr;
+	tr.translate = {hitPoint.x, hitPoint.y + 0.5f, hitPoint.z};
+	tr.scale = {1.0f, 1.0f, 1.0f};
+	renderer->DrawMesh(previewModelHandle_, previewTextureHandle_, tr, {0.6f, 1.0f, 0.6f, 0.6f}, "Toon");
+}
+
+void PahaseSystemScript::SpawnPlacedObject(GameScene* scene, const Engine::Vector3& hitPoint, const std::string& objPath) {
+	auto* renderer = scene->GetRenderer();
+	if (!renderer) return;
+
+ if (previewModelHandle_ == 0 || previewObjPath_ != objPath) {
+		previewModelHandle_ = renderer->LoadObjMesh(objPath);
+		previewObjPath_ = objPath;
+	}
+	if (previewTextureHandle_ == 0) {
+		previewTextureHandle_ = renderer->LoadTexture2D("Resources/white1x1.png");
+	}
+
+	SceneObject newObj;
+ newObj.name = (objPath.find("cylinder") != std::string::npos || objPath.find("Cylinder") != std::string::npos) ? "PlacedCylinder" : "PlacedCube";
+	newObj.translate = {hitPoint.x, hitPoint.y + 0.5f, hitPoint.z};
+	newObj.scale = {1.0f, 1.0f, 1.0f};
+	newObj.color = {1.0f, 1.0f, 1.0f, 1.0f};
+   newObj.modelPath = objPath;
+	newObj.texturePath = "Resources/white1x1.png";
+	newObj.modelHandle = previewModelHandle_;
+	newObj.textureHandle = previewTextureHandle_;
+
+	MeshRendererComponent mr;
+	mr.modelHandle = newObj.modelHandle;
+	mr.textureHandle = newObj.textureHandle;
+	mr.modelPath = newObj.modelPath;
+	mr.texturePath = newObj.texturePath;
+	mr.shaderName = "Toon";
+	newObj.meshRenderers.push_back(mr);
+
+	scene->SpawnObject(newObj);
 }
 
 void PahaseSystemScript::OnDestroy(SceneObject& /*obj*/, GameScene* /*scene*/) {}
--- Game/Scripts/PahaseSystemScript.h
+++ Game/Scripts/PahaseSystemScript.h
@@ -1,7 +1,12 @@
 #pragma once
 #include "IScript.h"
+#include <cstdint>
+#include <string>
 
 struct ImVec2; // 前方宣言
+namespace Engine {
+struct Vector3;
+}
 
 namespace Game {
 
@@ -11,7 +16,10 @@ public:
 	void Update(SceneObject& obj, GameScene* scene, float dt) override;
 	void OnDestroy(SceneObject& obj, GameScene* scene) override;
 
-	void Installation(GameScene* scene);
+    void Installation(GameScene* scene, const std::string& objPath);
+	bool TryGetTerrainHitPoint(GameScene* scene, Engine::Vector3& outHitPoint) const;
+   void DrawPlacementPreview(GameScene* scene, const Engine::Vector3& hitPoint, const std::string& objPath);
+	void SpawnPlacedObject(GameScene* scene, const Engine::Vector3& hitPoint, const std::string& objPath);
 
 	static bool IsPreparation() { return isPreparation_; };
 
@@ -19,6 +27,13 @@ private:
 	static bool isPreparation_;
 
 	bool preKeyP_ = false; // 初期化しておく
+    bool preKey1_ = false;
+	bool preKey2_ = false;
+	bool isPlacementMode_ = false;
+   std::string selectedObjPath_ = "Resources/cube/cube.obj";
+	std::string previewObjPath_;
+	uint32_t previewModelHandle_ = 0;
+	uint32_t previewTextureHandle_ = 0;
 };
 
 } // namespace Game
