712a456e8abaa3b9870d27cad75fb26cc85f301d
HakutoHenmi <k024g0044@g.neec.ac.jp>
2026-03-25 14:56:06 +0900

    読み取りの修正

--- Engine/Renderer.cpp
+++ Engine/Renderer.cpp
@@ -1426,9 +1426,34 @@ float4 main(float4 svpos:SV_POSITION, float3 worldPos:TEXCOORD0, float3 normal:T
 	if (!vsInst || !psInst || !CreatePSO("Instanced", vsInst.Get(), psInst.Get()))
 		return false;
 
-	// パーティクル インスタンス描画 (PixelShaderは既存のParticlePSを使用)
-	auto vsPartInst = CompileShaderFromFile(L"Resources/shaders/InstancedParticleVS.hlsl", "main", "vs_5_0");
-	auto psPart = CompileShaderFromFile(L"Resources/shaders/ParticlePS.hlsl", "main", "ps_5_0");
+	// パーティクル インスタンス描画 (インラインコンパイルで環境非依存にする)
+	static const char* kVSParticleInstanced = R"(
+struct InstanceData { row_major float4x4 world; float4 color; float4 uvScaleOffset; };
+StructuredBuffer<InstanceData> gInstanceData : register(t2);
+cbuffer CBFrame : register(b0) { row_major float4x4 gView; row_major float4x4 gProj; row_major float4x4 gViewProj; float3 gCamPos; float gTime; };
+struct VSOutput { float4 svpos : SV_POSITION; float2 uv : TEXCOORD; float4 color : COLOR; };
+VSOutput main(float4 pos : POSITION, float3 normal : NORMAL, float2 uv : TEXCOORD, uint instanceID : SV_InstanceID) {
+    VSOutput output;
+    InstanceData data = gInstanceData[instanceID];
+    output.svpos = mul(pos, mul(data.world, mul(gView, gProj)));
+    output.uv.x = uv.x * data.uvScaleOffset.x + data.uvScaleOffset.z;
+    output.uv.y = uv.y * data.uvScaleOffset.y + data.uvScaleOffset.w;
+    output.color = data.color;
+    return output;
+}
+)";
+	static const char* kPSParticle = R"(
+Texture2D<float4> tex : register(t0);
+SamplerState smp : register(s0);
+float4 main(float4 svpos:SV_POSITION, float2 uv:TEXCOORD, float4 color:COLOR) : SV_TARGET {
+    float4 texColor = tex.Sample(smp, uv);
+    texColor *= color;
+    if (texColor.a <= 0.0f) { discard; }
+    return texColor;
+}
+)";
+	auto vsPartInst = CompileShader(kVSParticleInstanced, "main", "vs_5_0");
+	auto psPart = CompileShader(kPSParticle, "main", "ps_5_0");
 	if (!vsPartInst || !psPart || !CreatePSO("ParticleInstanced", vsPartInst.Get(), psPart.Get()))
 		return false;
 
