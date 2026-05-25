43494b31cfa1b3d0db4d32a2ef366789a29a80d6
剛 増谷 <K024G0075@g.neec.ac.jp>
2026-05-25 11:13:51 +0900

    こみっと

--- Game/Scripts/Enemy/BaseEnemy.h
+++ Game/Scripts/Enemy/BaseEnemy.h
@@ -14,20 +14,6 @@ enum EnemyCategory {
 	Other
 };
 
-// エネミーそれぞれが持つ戦術のロール
-enum EnemyRole {
-	Tank,
-	Attacker,
-	Ranged,
-};
-
-// 戦術の状態
-enum TacticalState {
-	Advancing,
-	WaitingForTank,
-	Engaging,
-};
-
 //敵の挙動になる基底クラス
 namespace Game {
 
@@ -102,11 +88,6 @@ protected: // メンバ変数
 	float scanTimer_ = 0.0f;
 	float groundHeight_ = 0.0f;	// FlyTypeが地面の高さを取るため
 
-	//戦術面
-	EnemyRole role_ = EnemyRole::Attacker;	// 初期値はアタッカー
-	TacticalState tacticalState_ = TacticalState::Advancing;	// 初期値は進軍
-	float waitTimer_ = 0.0f;
-	float maxWaitTimer_ = 8.0f;	// 最大待機時間
 	float frozenParticleTimer_ = 0.0f; // 凍結状態時の冷気噴射タイマー
 	float poisonedParticleTimer_ = 0.0f; // 毒状態時の泡噴射タイマー
 };
